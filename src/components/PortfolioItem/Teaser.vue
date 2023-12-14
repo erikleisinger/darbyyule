f1d393b226b2e9c53ee3a076e7a9607dfb8164a8
erikleisinger <erikleisinger@gmail.com>
2023-12-13 19:29:32 -0500

    badges added to content cards

--- src/components/PortfolioItem/Teaser.vue
+++ src/components/PortfolioItem/Teaser.vue
@@ -20,6 +20,11 @@
       data-flip-id="image"
       :id="`teaser-image-${identifier}`"
     ></div>
+    <div class="badges--container">
+      <div class="badge" v-for="badge in BADGES_FOR_CARD[props.name]" :key="badge">
+       {{badge}}
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -76,10 +81,44 @@
     background-position-x: center !important;
     background-size: 100% !important;
   }
+  .badges--container {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    margin-bottom: calcDimensionXs(8px);
+    @include sm {
+      margin-bottom: calcDimension(10px)
+    }
+    .badge{
+      border-radius: 6px;
+      background-color: #FFFCF8;
+      font-family: $font-title;
+      padding: 0.1em;
+      font-size: calcDimensionXs(10px);
+      width: calcDimensionXs(80px);
+      height: calcDimensionXs(20px);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-right: calcDimensionXs(10px);
+      padding-top: calcDimensionXs(3px);
+      
+      @include sm {
+        font-size: calcDimension(18px);
+        width: calcDimension(144px);
+        height: calcDimension(35px);
+        margin-right: calcDimension(18px);
+        padding-top: calcDimension(4px);
+         border-radius: 10px;
+      }
+    }
+  }
 }
 </style>
 <script setup>
-import { SECTION_TILE_IMAGES } from '@/constants/content'
+
+import { SECTION_NAMES, SECTION_TILE_IMAGES } from '@/constants/content'
 import { onMounted, ref } from 'vue'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
@@ -91,6 +130,22 @@ const props = defineProps({
   odd: Boolean,
   title: String
 })
+
+const BADGES = {
+  ANALYSIS: 'Analysis',
+  CONTENT_STRAT: 'Content Strat',
+  DESIGN_STRAT: 'Design Strat',
+  USER_RESEARCH: 'User Research',
+  UX_WRITING: 'UX Writing',
+  VISUAL_DESIGN: 'Visual Design'
+}
+
+const BADGES_FOR_CARD = {
+[SECTION_NAMES.COUNSELLING]: [BADGES.DESIGN_STRAT, BADGES.USER_RESEARCH, BADGES.VISUAL_DESIGN],
+[SECTION_NAMES.DARKHORSE]: [BADGES.USER_RESEARCH, BADGES.ANALYSIS],
+[SECTION_NAMES.FOODHUB]: [BADGES.USER_RESEARCH, BADGES.VISUAL_DESIGN],
+[SECTION_NAMES.CONTENT]: [BADGES.CONTENT_STRAT, BADGES.UX_WRITING, BADGES.USER_RESEARCH]
+}
 
 const img = new URL('/images/' + SECTION_TILE_IMAGES[props.name], import.meta.url).href
 
